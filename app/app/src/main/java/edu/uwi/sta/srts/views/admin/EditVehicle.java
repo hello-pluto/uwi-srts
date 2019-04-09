6424f3e4926631d4a63ea2b30f0ec945ba906f2b
Amanda Seenath <amanda.seenath@hotmail.com>
2019-04-09 12:17:23 -0400

    Added adding/editing of vehicle

---
+++ app/app/src/main/java/edu/uwi/sta/srts/views/admin/EditVehicle.java
@@ -0,0 +1,15 @@
+package edu.uwi.sta.srts.views.admin;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import edu.uwi.sta.srts.R;
+
+public class EditVehicle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_vehicle);
+    }
+}
